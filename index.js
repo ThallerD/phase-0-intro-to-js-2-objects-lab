c406b4c11bfe24c4e1c1333fd7d9508ee978f751
David Thaller <golf.nautica@gmail.com>
2023-03-16 17:10:25 -0700

    commit tree 0742be154da6ad7f3e03a5eda074a5df2a40d946

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Write your solution in this file!
+
+
+const employee = {
+    name: "adam",
+    streetAddress: "123 A Street",
+};
+
+console.log(employee);
+
+console.log(employee["name"]);
+
+console.log(employee.name);
+
+function updateEmployeeWithKeyAndValue (employee, key, value) {
+    const updatedEmployee = {...employee};
+
+    updatedEmployee[key] = value;
+
+    return updatedEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key]=value; 
+
+    return employee
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = {...employee};
+
+    delete newEmployee.name;
+
+    return newEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee.name;
+
+    return employee;
+}
